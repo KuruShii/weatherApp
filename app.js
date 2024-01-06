bedd2abc2a1e71b248c7557426384afdfdf1eadb
KuruSh1 <cassinab21@gmail.com>
2024-01-06 12:35:45 +0000

    Finished up website design and code :D

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const apiKey = "c6705d4a1bc1422eb17161310240501";
 const link = "http://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=";
 
+let fahrenheit = false;
+
 async function weatherCall(input) {
     // Fetches weather data from the api
     const result = await fetch(link + input);
@@ -29,16 +31,54 @@ const time = document.getElementById("time");
 const ic = document.getElementById("con");
 const con = document.getElementById("con5");
 
+async function weatherDisplay(input) {
+    console.log(input)
+    if (input.value) {
+      input = input.value;  
+      console.log(input)
+    } else if (typeof input.innerHTML === "string") {
+      input = input.innerHTML
+    } else {
+      console.log(input.coords.latitude)
+      input = input.coords.latitude + " " + input.coords.longitude;
+    } 
+    const weather = await weatherReturn(input);
+    document.getElementById("input").value = "";
+    loc.innerHTML = weather.location.name + ", " + weather.location.country;
+    temp.innerHTML = fahrenheit ? weather.current.temp_f + "°F" : weather.current.temp_c + "°C";
+    hum.innerHTML = weather.current.humidity + "%";
+    const tim = weather.current.last_updated.split(" ")[1];
+    if (tim.split(":")[0] > 18 || tim.split(":")[0] < 7) {
+      document.querySelector("body").style.backgroundImage = "url('imgs/night.png')";
+    }
+    else {
+      document.querySelector("body").style.backgroundImage = "url('imgs/day.png')";
+    }
+}
 
 button.addEventListener('click', async () => {
-    const input = document.getElementById("input");
-    const weather = await weatherReturn(input.value);
-    loc.innerHTML = weather.location.name + ", " + weather.location.country;
-    temp.innerHTML = weather.current.temp_c;
-    hum.innerHTML = weather.current.humidity;
-    feel.innerHTML = weather.current.feelslike_c; 
-    time.innerHTML = weather.current.last_updated;
-    ic.src = weather.current.condition.icon;
-    con.innerHTML = weather.current.condition.text;
+  const input = document.getElementById("input");
+  weatherDisplay(input);
 });
-    
+
+addEventListener("DOMContentLoaded", async () => {
+  if(navigator.geolocation) {
+    console.log(navigator.geolocation.getCurrentPosition(weatherDisplay))
+  } else {
+    await weatherDisplay("Washington");
+  }
+});
+
+const f = document.getElementById("f");
+f.addEventListener('click', () => {
+  if(fahrenheit) {
+    fahrenheit = false;
+    f.innerHTML = "F";
+    weatherDisplay(loc);
+  } else {
+    fahrenheit = true;
+    f.innerHTML = "C";
+    console.log(fahrenheit);
+    weatherDisplay(loc);
+  }
+});
